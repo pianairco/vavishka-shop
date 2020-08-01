16e2d3cf5470d10d539e3e528955da7bf8f969ca
pianairco <pianairco@gmail.com>
2020-08-01 13:17:29 -0700

    store

--- src/main/java/ir/piana/dev/strutser/util/JspUtil.java
+++ src/main/java/ir/piana/dev/strutser/util/JspUtil.java
@@ -7,7 +7,24 @@ public class JspUtil {
     public static String getStore(String storeName) {
         StringBuffer sb = new StringBuffer();
         sb.append("var " + storeName == null || storeName.isEmpty() ? "store" : storeName + "={\n" +
-                "        state:{forms: {}},\n" +
+                "        state:{forms: Object},\n" +
+                "        setToForms: function(formName, propertyName, property) {\n" +
+                "            let obj = Object.assign({}, this.state.forms[formName]);\n" +
+                "            obj[propertyName] = property;\n" +
+                "            this.state.forms[formName] = obj;\n" +
+                "        }\n" +
+                "    }");
+        return Arrays.stream(sb.toString().split("\n")).map(String::trim)
+                .collect(Collectors.joining());
+    }
+
+    public static String getStore(String storeName, String... forms) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("var " + storeName == null || storeName.isEmpty() ? "store" : storeName + "={\n" +
+                "        state:{forms: {");
+                for(String form : forms)
+                    sb.append(form).append(": {},");
+                sb.append("}},\n" +
                 "        setToForms: function(formName, propertyName, property) {\n" +
                 "            let obj = Object.assign({}, this.state.forms[formName]);\n" +
                 "            obj[propertyName] = property;\n" +
